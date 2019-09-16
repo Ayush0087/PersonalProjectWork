256727e378b6e8794247931cee6ee09184ba6212
Ayush0087 <54570255+Ayush0087@users.noreply.github.com>
2019-09-16 18:07:02 +0530

    jgf
    jhgk

---
+++ till monday/GreatOutdoor ReturnOnlineOrder/GreatOutdoor.Entity/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GreatOutdoor.Entity
+{
+    public class OnlineReturn
+    {
+        //fields
+        private static int _returnID;
+        private string _PurposeofReturn;
+        private string _noOfReturn;
+        private DateTime _dateOfReturn;
+
+
+       
+
+        public DateTime DateOfReturn { get => DateOfReturn; set => DateOfReturn = value; }
+        public static int ReturnID { get => _returnID; set => _returnID = value; }
+        public string PurposeofReturn { get => _PurposeofReturn; set => _PurposeofReturn = value; }
+        public string NoOfReturn { get => _noOfReturn; set => _noOfReturn = value; }
+        
+        //constructor
+        public OnlineReturn()
+        {
+            _PurposeofReturn = string.Empty;
+            _noOfReturn = string.Empty;
+            _returnID = 0;
+        }
+
+
+
+       
+    }
+}
